cbc10a0bf611e0c1ee0f0e5aa2ee3b661cc60cc3
Havoker <108506748+Havoker@users.noreply.github.com>
2023-06-26 13:02:05 +1000

    Added som thangs

---
+++ lib/category.dart
@@ -0,0 +1,32 @@
+class Category {
+  final String categoryName;
+  final List<Question> questions;
+
+  Category.fromJson(Map<String, dynamic> json)
+  :
+    categoryName = json['category_name'],
+    questions = List<Question>.from(json['questions'].map((question) => Question.fromJson(question)));
+
+  Map<String, dynamic> toJson() =>
+      {  
+        'category_name': categoryName,
+        'questions': questions,
+      };
+}
+
+class Question {
+  final String full;
+  final String short;
+  final String type;
+  final String id;
+
+  Question.fromJson(Map<String, dynamic> json)
+  :
+    full = json['full_question'],
+    short = json['short_question'],
+    type = json['type'],
+    id = json['id'];
+
+  Question(this.full, this.short, this.type, this.id);
+  
+}
---
+++ lib/questions_tab.dart
@@ -0,0 +1,282 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:kit301_ears/category.dart';
+
+class QuestionsTab extends StatefulWidget {
+  const QuestionsTab({super.key});
+
+  @override
+  State<QuestionsTab> createState() => _QuestionsTabState();
+}
+
+class _QuestionsTabState extends State<QuestionsTab> {
+  List<Category> _categoryList = [];
+
+  Future initCategories() async{
+    List<Category> tempCategoryList = [];
+
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+
+    final categoryPaths = manifestMap.keys
+        .where((String key) => key.contains('categories/'))
+        .where((String key) => key.contains('.json'))
+        .toList();
+
+    for (var path in categoryPaths) {
+      final rawJson = await rootBundle.loadString(path);
+      final category = Category.fromJson(jsonDecode(rawJson));
+      tempCategoryList.add(category);
+    }
+
+    setState(() {
+      _categoryList = tempCategoryList;
+    });
+  }
+
+  @override
+  void initState() {    
+    super.initState();
+    initCategories();
+  }
+
+  int _selectedIndex = 0;
+  int _selectedItemIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_categoryList.isEmpty) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                margin: EdgeInsets.zero,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final isLastItem = index == _categoryList.length - 1;
+                    return Container(
+                      margin: EdgeInsets.fromLTRB(8, 8, 8, isLastItem ? 8 : 0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 2
+                          ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: ListTile(
+                        tileColor: Colors.grey[300],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        title: Text(
+                          _categoryList[index].categoryName,
+                          style: TextStyle(
+                            color: index == _selectedIndex ? Theme.of(context).scaffoldBackgroundColor : Colors.black,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        selected: index == _selectedIndex,
+                        selectedTileColor: Colors.redAccent[100],
+                        trailing: index == _selectedIndex
+                            ? Icon(Icons.arrow_forward_ios_rounded, color: Theme.of(context).scaffoldBackgroundColor)
+                            : null,
+                        onTap: () {
+                          setState(() {
+                            _selectedIndex = index;
+                            _selectedItemIndex = -1;
+                          });
+                        },
+                      ),
+                    );
+                  },
+                  itemCount: _categoryList.length,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2
+                  ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Card(
+                margin: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    List<Question> _CategoryItems = _categoryList[_selectedIndex].questions;
+                      final isLastItem = index == _CategoryItems.length - 1;
+                      return Container(
+                        margin: EdgeInsets.fromLTRB(8, 8, 8, isLastItem ? 8 : 0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 2
+                            ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: ListTile(
+                          tileColor: Colors.grey[300],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          title: Text(
+                            _CategoryItems[index].short,
+                            style: TextStyle(
+                              color: index == _selectedItemIndex ? Theme.of(context).scaffoldBackgroundColor : Colors.black,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          selected: index == _selectedItemIndex,
+                          selectedTileColor: Colors.redAccent[100],
+                          onTap: () {
+                            setState(() {
+                              _selectedItemIndex = index;
+                            });
+                            showDialog(
+                              context: context,
+                              barrierColor: Colors.black.withOpacity(0.75),
+                              builder: (BuildContext context) {
+                                return Dialog(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: WillPopScope(
+                                    onWillPop: () async {
+                                      setState(() {
+                                        _selectedItemIndex = -1;
+                                      });
+                                      return true;
+                                    },
+                                    child: Padding(
+                                      padding: EdgeInsets.all(4),
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width * 0.7, // Adjust width as needed
+                                        height: MediaQuery.of(context).size.height * 0.7, // Adjust height as needed
+                                        decoration: BoxDecoration(
+                                          border: Border.all(
+                                            color: Colors.grey,
+                                            width: 2
+                                            ),
+                                          borderRadius: BorderRadius.circular(8.0),
+                                        ),
+                                        padding: const EdgeInsets.fromLTRB(30.0, 25.0, 30.0, 25.0),
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              _CategoryItems[index].short,
+                                              style: const TextStyle(
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 20.0),
+                                            Text(
+                                              _CategoryItems[index].full,
+                                              style: const TextStyle(fontSize: 16.0),
+                                            ),
+                                            const SizedBox(height: 20.0),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                ElevatedButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      _selectedItemIndex = -1;
+                                                    });
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  style: ElevatedButton.styleFrom(
+                                                    padding: const EdgeInsets.symmetric(
+                                                      vertical: 30.0,
+                                                      horizontal: 60.0,
+                                                    ),
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(10.0),
+                                                    ),
+                                                    backgroundColor: Colors.redAccent,
+                                                    ),
+                                                  child: const Text(
+                                                    'Cancel',
+                                                    style: TextStyle(fontSize: 18.0),
+                                                  ),
+                                                ),
+                                                ElevatedButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                    Future.delayed(const Duration(seconds: 5), () {
+                                                      setState(() {
+                                                        _selectedItemIndex = -1;
+                                                      });
+                                                    });
+                                                  },
+                                                  style: ElevatedButton.styleFrom(
+                                                    padding: const EdgeInsets.symmetric(
+                                                      vertical: 30.0,
+                                                      horizontal: 60.0,
+                                                    ),
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(10.0),
+                                                    ),
+                                                    backgroundColor: Colors.green
+                                                  ),
+                                                  child: const Text(
+                                                    'Confirm',
+                                                    style: TextStyle(fontSize: 18.0),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      );
+                  },
+                  itemCount: _categoryList[_selectedIndex].questions.length,               
+                ),
+              )
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
