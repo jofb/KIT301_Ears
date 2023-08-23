d3003167322edd02d375a8d70a008058cea47186
Jordan <jordanwyldebrowne@gmail.com>
2023-08-23 19:41:51 +1000

    refactor audio loading

--- lib/settings.dart
+++ lib/settings.dart
@@ -23,6 +23,8 @@ class _SettingsWidgetState extends State<SettingsWidget>
     with TickerProviderStateMixin {
   late AnimationController audioProgressController;
 
+  bool downloadingAudio = false;
+
   void showSnackbar(String msg, Color colour) {
     // create snack bar
     final snackBar = SnackBar(
@@ -37,7 +39,6 @@ class _SettingsWidgetState extends State<SettingsWidget>
     );
 
     // show snackbar using scaffold messenger
-
     widget.scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
   }
 
@@ -145,78 +146,80 @@ class _SettingsWidgetState extends State<SettingsWidget>
               CustomSettingsTile(
                 child: Row(
                   children: [
-                    Column(
-                      children: [
-                        SettingsButton(
-                          text: 'Download audio',
-                          onPressed: () async {
-                            // get language labels
-                            List<String> labels = languageModel.labels
-                                .map((l) => l['code']!)
-                                .toList();
+                    SettingsButton(
+                      text: 'Download audio',
+                      onPressed: () async {
+                        setState(() {
+                          downloadingAudio = true;
+                        });
+                        // get language labels
+                        List<String> labels = languageModel.labels
+                            .map((l) => l['code']!)
+                            .toList();
 
-                            final appDir =
-                                await getApplicationDocumentsDirectory();
-                            // ensure audio directory exists
-                            final audioDir = Directory("${appDir.path}/audio");
+                        final appDir = await getApplicationDocumentsDirectory();
+                        // ensure audio directory exists
+                        final audioDir = Directory("${appDir.path}/audio");
 
-                            if (!await Directory(audioDir.path).exists()) {
-                              await Directory(audioDir.path).create();
-                            }
+                        if (!await Directory(audioDir.path).exists()) {
+                          await Directory(audioDir.path).create();
+                        }
 
-                            // get the firebase instance
-                            var instance = FirebaseStorage.instance.ref("/");
+                        List<DownloadTask> downloads = [];
 
-                            // used for progress indicator
-                            int total = 0;
-                            int progress = 0;
+                        // get the firebase instance
+                        var instance = FirebaseStorage.instance.ref("/");
 
-                            // now search the the instance for all our labels
-                            for (String label in labels) {
-                              // ensure labelled directory exists
-                              final labelDir =
-                                  Directory("${audioDir.path}/$label");
+                        // // used for progress indicator
+                        // int total = 0;
+                        // int progress = 0;
 
-                              if (!await Directory(labelDir.path).exists()) {
-                                await Directory(labelDir.path).create();
-                              }
+                        // now search the the instance for all our labels
+                        for (String label in labels) {
+                          // ensure labelled directory exists
+                          final labelDir = Directory("${audioDir.path}/$label");
 
-                              ListResult list =
-                                  await instance.child(label).list();
+                          if (!await Directory(labelDir.path).exists()) {
+                            await Directory(labelDir.path).create();
+                          }
 
-                              total += list.items.length;
-                              // now for every item we download and place in folder
-                              for (Reference item in list.items) {
-                                String filePath =
-                                    "${labelDir.path}/${item.name}";
-                                // finally download
-                                File file = File(filePath);
-                                final downloadTask = item.writeToFile(file);
-                                downloadTask.snapshotEvents.listen((event) {
-                                  if (event.state == TaskState.success) {
-                                    progress += 1;
-                                    audioProgressController.value =
-                                        progress / total;
-                                    if (progress == total) {
-                                      showSnackbar(
-                                          'All Audio Downloaded', Colors.green);
-                                    }
-                                  }
-                                });
-                              }
-                            }
-                          },
-                          theme: themeModel.currentTheme,
-                        ),
-                        if (audioProgressController.status ==
-                            AnimationStatus.forward)
-                          SizedBox(
-                            width: 200,
-                            child: LinearProgressIndicator(
-                                value: audioProgressController.value),
-                          )
-                      ],
+                          ListResult list = await instance.child(label).list();
+
+                          // total += list.items.length;
+                          // now for every item we download and place in folder
+                          for (Reference item in list.items) {
+                            String filePath = "${labelDir.path}/${item.name}";
+                            // finally download
+                            File file = File(filePath);
+                            final downloadTask = item.writeToFile(file);
+                            downloads.add(downloadTask);
+                            // downloadTask.snapshotEvents.listen((event) {
+                            //   if (event.state == TaskState.success) {
+                            //     progress += 1;
+                            //     audioProgressController.value =
+                            //         progress / total;
+                            //   }
+                            // });
+                          }
+                        }
+                        Future.wait(downloads).then((value) {
+                          if (mounted) {
+                            setState(() {
+                              downloadingAudio = false;
+                            });
+                          }
+
+                          showSnackbar('All Audio Downloaded', Colors.green);
+                        });
+                      },
+                      theme: themeModel.currentTheme,
                     ),
+                    if (downloadingAudio)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Transform.scale(
+                            scale: 0.8, child: CircularProgressIndicator()),
+                      ),
                   ],
                 ),
               ),
